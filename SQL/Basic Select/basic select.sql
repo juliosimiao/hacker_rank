4e468cf0b1b02f0014764377a00a0651c79d05ee
MIKROPIS\Julio <julio_simiao@hotmail.com>
2017-08-01 15:41:31 +0200

    Alteracoes realizadas ate o exercicio Weather Observation Station 4

--- SQL/Basic Select/basic select.sql
+++ SQL/Basic Select/basic select.sql
@@ -9,3 +9,18 @@ select * from city;
 
 /* Select By ID */
 select * from city where id = "1661";
+
+/* Japanese Cities' Attributes */
+select * from city where countrycode = "JPN";
+
+/* Japanese Cities' Names */
+select name from city where countrycode = "JPN";
+
+/* Weather Observation Station 1 */
+select city,state from station;
+
+/* Weather Observation Station 3 */
+select DISTINCTcity from station where (id % 2 == 0)
+
+/* Weather Observation Station 4 */
+select count(city) - count(distinct city) from station;
