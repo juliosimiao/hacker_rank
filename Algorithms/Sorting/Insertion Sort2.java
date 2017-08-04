a089221cc37d70f6bc1d9ff1c19f76f004ecb7d2
MIKROPIS\Julio <julio_simiao@hotmail.com>
2017-08-04 15:06:39 +0200

    Correcao Insertion Sort2

--- Algorithms/Sorting/Insertion Sort2.java
+++ Algorithms/Sorting/Insertion Sort2.java
@@ -1,13 +1,13 @@
-import java.io.;
-import java.util.;
+import java.io.*;
+import java.util.*;
 
 public class Solution {
 
     public static void insertionSortPart2(int[] ar)
     {       
-           for(int i = 1; i  ar.length; i++){
-               for(int j = i; j = 1; j--){
-                   if(ar[j]  ar[j-1]){
+           for(int i = 1; i < ar.length; i++){
+               for(int j = i; j >= 1; j--){
+                   if(ar[j] < ar[j-1]){
                        int aux = ar[j-1];
                        ar[j-1] = ar[j];
                        ar[j] = aux;
@@ -23,16 +23,16 @@ public class Solution {
         Scanner in = new Scanner(System.in);
        int s = in.nextInt();
        int[] ar = new int[s];
-       for(int i=0;is;i++){
+       for(int i=0;i<s;i++){
             ar[i]=in.nextInt(); 
        }
        insertionSortPart2(ar);    
                     
     }    
     private static void printArray(int[] ar) {
-      for(int n ar){
-         System.out.print(n+ );
+      for(int n: ar){
+         System.out.print(n+" ");
       }
-        System.out.println();
+        System.out.println("");
    }
 }
