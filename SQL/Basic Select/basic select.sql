f909c0404d599ddfaa7c72fdc4b1b0502b7f4082
MIKROPIS\Julio <julio_simiao@hotmail.com>
2017-08-04 14:44:22 +0200

    All the answer for basic select

--- SQL/Basic Select/basic select.sql
+++ SQL/Basic Select/basic select.sql
@@ -20,8 +20,41 @@ select name from city where countrycode = "JPN";
 select city,state from station;
 
 /* Weather Observation Station 3 */
-select DISTINCTcity from station where (id % 2 == 0)
+select DISTINCT city from station where (id % 2 == 0)
 
 /* Weather Observation Station 4 */
 select count(city) - count(distinct city) from station;
 
+/* Weather Observation Station 5 */
+select city,char_length(city) from station order by char_length(city),city asc limit 1;
+select city,char_length(city) from station order by char_length(city) desc,city asc limit 1;
+
+/* Weather Observation Station 6 */
+select distinct city from station where city RLIKE '^[aeiouAEIOU]';
+
+/* Weather Observation Station 7 */
+select distinct city from station where city RLIKE '[aeiouAEIOU]$';
+
+/* Weather Observation Station 8 */
+select distinct city from station where city RLIKE '^[aeiouAEIOU]' and city RLIKE '[aeiouAEIOU]$';
+
+/* Weather Observation Station 9 */
+select distinct city from station where city RLIKE '^[^aeiouAEIOU]';
+
+/* Weather Observation Station 10 */
+select distinct city from station where city RLIKE '[^aeiouAEIOU]$';
+
+/* Weather Observation Station 11 */
+select distinct city from station where city RLIKE '^[^aeiouAEIOU]' or city RLIKE '[^aeiouAEIOU]$';
+
+/* Weather Observation Station 12 */
+select distinct city from station where city RLIKE '^[^aeiouAEIOU]' and city RLIKE '[^aeiouAEIOU]$';
+
+/* Higher Than 75 Marks */
+select name from students where Marks > 75 order by RIGHT(name, 3) ASC,id asc;
+
+/* Employee Names */
+select name from employee order by name asc;
+
+/* Employee Salaries */
+select name from employee where salary > 2000 and months < 10;
